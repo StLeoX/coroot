c44841f5c1e231cef15eea791f25190d89b44215
StLeoX <stleox@qq.com>
2024-12-26 10:53:27 +0800

    Change ServerSpanService.Upload to client streaming approach.

--- grpc/event.go
+++ grpc/event.go
@@ -1,24 +1,43 @@
 package grpc
 
 import (
-	"context"
-	v1 "github.com/coroot/coroot/api/proto/coroot/service/v1"
+	"io"
+	"k8s.io/klog"
+
+	corootv1 "github.com/coroot/coroot/api/proto/coroot/service/v1"
 	"github.com/coroot/coroot/collector"
 	"github.com/coroot/coroot/collector/event"
 )
 
-type EventServiceServer struct {
-	v1.UnimplementedEventServiceServer
+type ServerSpanServiceServer struct {
+	corootv1.UnimplementedServerSpanServiceServer
 	batcher *event.ServerSpansBatch
 }
 
-func (es *EventServiceServer) Upload(_ context.Context, req *v1.EventServiceUploadRequest) (*v1.EventServiceUploadResponse, error) {
-	es.batcher.Add(req)
-	return &v1.EventServiceUploadResponse{}, nil
+func (es *ServerSpanServiceServer) Upload(stream corootv1.ServerSpanService_UploadServer) error {
+	resp := &corootv1.ServerSpanServiceUploadResponse{}
+
+	for {
+		req, err := stream.Recv()
+		// client stream closed
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			klog.Warningln(err)
+			return err
+		}
+		es.batcher.Add(req.Span)
+	}
+
+	err := stream.SendAndClose(resp)
+	if err != nil {
+		klog.Warningln(err)
+		return err
+	}
+	return nil
 }
 
-func NewEventServiceServer(coll *collector.Collector) *EventServiceServer {
-	return &EventServiceServer{
-		batcher: coll.GetServerSpansBatch(coll.GetCurrentProjectId()),
-	}
+func NewServerSpanServiceServer(coll *collector.Collector) *ServerSpanServiceServer {
+	return &ServerSpanServiceServer{batcher: coll.GetServerSpansBatch(coll.GetCurrentProjectId())}
 }
